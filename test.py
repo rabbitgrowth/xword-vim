c97155d3d305c4760ab698763c1ccfa60076c12a
Nick Yeow <ytknick@gmail.com>
2022-03-12 10:43:29 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,84 @@
+import itertools
+import operator
+import unittest
+
+import xword
+
+class TestHelpers(unittest.TestCase):
+    def test_sign(self):
+        self.assertEqual(xword.sign(2),   1)
+        self.assertEqual(xword.sign(1),   1)
+        self.assertEqual(xword.sign(0),   0)
+        self.assertEqual(xword.sign(-1), -1)
+        self.assertEqual(xword.sign(-2), -1)
+
+class TestMiniPuzzle(unittest.TestCase):
+    def setUp(self):
+        grid = [[None if char == '.' else char for char in line]
+                for line in ['.RACED.',
+                             'BELARUS',
+                             'LABTECH',
+                             'ALE.CHE',
+                             'KIRSTIE',
+                             'ESTREET',
+                             '.MAIDS.']]
+
+        self.grid = xword.Grid(grid)
+
+    def test_width_and_height(self):
+        self.assertEqual(self.grid.width,  7)
+        self.assertEqual(self.grid.height, 7)
+
+    def test_square_runs(self):
+        self.assertEqual([''.join(square.solution for square in word.squares)
+                          for word in self.grid.words[xword.Direction.ACROSS]],
+                         ['RACED', 'BELARUS', 'LABTECH', 'ALE', 'CHE', 'KIRSTIE', 'ESTREET', 'MAIDS'])
+        self.assertEqual([''.join(square.solution for square in word.squares)
+                          for word in self.grid.words[xword.Direction.DOWN]],
+                         ['REALISM', 'ALBERTA', 'CAT', 'ERECTED', 'DUCHIES', 'BLAKE', 'SHEET', 'SRI'])
+
+    def test_word_links(self):
+        word = self.grid.words[xword.Direction.ACROSS][0]
+        self.assertIsNone(word.prev)
+        self.assertIs(word.next.prev, word)
+        self.assertEqual(''.join(square.solution for square in word.next.squares), 'BELARUS')
+
+    def test_square_links(self):
+        square = self.grid.get(1, 0)
+        self.assertIsNone(square.prev[xword.Direction.ACROSS])
+        self.assertIs(square.next[xword.Direction.ACROSS].prev[xword.Direction.ACROSS], square)
+        for _ in range(10):
+            square = square.next[xword.Direction.ACROSS]
+        self.assertEqual(square.solution, 'U')
+        for _ in range(10):
+            square = square.next[xword.Direction.DOWN]
+        self.assertEqual(square.solution, 'E')
+
+    def test_up_down_left_right(self):
+        cursor = xword.Cursor(self.grid.get(2, 3), xword.Direction.ACROSS, self.grid)
+        self.assertEqual(cursor.h().square.coords, (1, 3))
+        self.assertEqual(cursor.j().square.coords, (2, 4))
+        self.assertEqual(cursor.k().square.coords, (2, 2))
+        self.assertEqual(cursor.l().square.coords, (4, 3)) # skip black square
+
+    def test_next_squares(self):
+        cursor = xword.Cursor(self.grid.get(2, 3), xword.Direction.ACROSS, self.grid)
+        self.assertEqual([(direction, ''.join(square.solution for square, _ in pairs))
+                          for direction, pairs in itertools.groupby(cursor.next_squares(), key=operator.itemgetter(1))],
+                         [(xword.Direction.ACROSS, 'CHEKIRSTIEESTREETMAIDS'),
+                          (xword.Direction.DOWN,   'REALISMALBERTACATERECTEDDUCHIESBLAKESHEETSRI'),
+                          (xword.Direction.ACROSS, 'RACEDBELARUSLABTECHAL')])
+
+    def test_word_motions(self):
+        cursor = xword.Cursor(self.grid.get(3, 0), xword.Direction.ACROSS, self.grid)
+        cursor = cursor.w()
+        self.assertEqual(cursor.square.coords, (0, 1))
+        cursor = cursor.b().b()
+        self.assertEqual(cursor.square.coords, (3, 4))
+        cursor = cursor.e().e().e()
+        self.assertEqual(cursor.square.coords, (6, 1))
+        cursor = cursor.ge().ge().ge().ge()
+        self.assertEqual(cursor.square.coords, (0, 5))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ xword.py
@@ -0,0 +1,187 @@
+from __future__ import annotations
+from collections.abc import Callable, Iterator
+import collections
+import enum
+import itertools
+
+def sign(n: int) -> int:
+    return (n > 0) - (n < 0)
+
+class Direction(enum.Enum):
+    ACROSS = enum.auto()
+    DOWN   = enum.auto()
+
+class Grid:
+    def __init__(self, grid: list[list[str | None]]) -> None:
+        self.grid = [[Square(x, y, solution) for x, solution in enumerate(row)]
+                     for y, row in enumerate(grid)]
+
+        self.width  = len(self.grid[0])
+        self.height = len(self.grid)
+
+        # Map starts of runs of white squares to those runs of white squares
+        starts: dict[Square, dict[Direction, list[Square]]] = collections.defaultdict(dict)
+        for direction, grid in zip(Direction, (self.grid, self.transpose())):
+            for row in grid:
+                for is_white, square_group in itertools.groupby(row, lambda square: square.is_white()):
+                    if is_white:
+                        square_list = list(square_group)
+                        start = square_list[0]
+                        starts[start][direction] = square_list
+
+        # Assign clue numbers to words
+        self.words: dict[Direction, list[Word]] = collections.defaultdict(list)
+        for clue_number, start in enumerate(sorted(starts, key=lambda square: (square.y, square.x)), start=1):
+            for direction in Direction:
+                squares = starts[start].get(direction)
+                if squares is not None:
+                    word = Word(squares, clue_number)
+                    self.words[direction].append(word)
+
+        # Doubly-link words and squares and link squares to words
+        for direction, words in self.words.items():
+            prev_word   = None
+            prev_square = None
+            for word in words:
+                if prev_word is not None:
+                    word.prev = prev_word
+                    prev_word.next = word
+                for square in word.squares:
+                    square.word[direction] = word
+                    if prev_square is not None:
+                        square.prev[direction] = prev_square
+                        prev_square.next[direction] = square
+                    prev_square = square
+                prev_word = word
+
+    def transpose(self) -> list[list[Square]]:
+        return list(map(list, zip(*self.grid)))
+
+    def within_bounds(self, x: int, y: int) -> bool:
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def get(self, x: int, y: int) -> Square | None:
+        if self.within_bounds(x, y):
+            return self.grid[y][x]
+        return None
+
+class Word:
+    def __init__(self, squares: list[Square], clue_number: int) -> None:
+        self.squares     = squares
+        self.clue_number = clue_number
+
+        self.prev: Word | None = None
+        self.next: Word | None = None
+
+class Square:
+    def __init__(self, x: int, y: int, solution: str | None) -> None:
+        self.x = x
+        self.y = y
+        self.solution = solution
+
+        self.prev: dict[Direction, Square | None] = {direction: None for direction in Direction}
+        self.next: dict[Direction, Square | None] = {direction: None for direction in Direction}
+
+        self.word: dict[Direction, Word] = {}
+
+    @property
+    def coords(self) -> tuple[int, int]:
+        return self.x, self.y
+
+    def is_black(self) -> bool:
+        return self.solution is None
+
+    def is_white(self) -> bool:
+        return not self.is_black()
+
+class Cursor:
+    def __init__(self, square: Square, direction: Direction, grid: Grid) -> None:
+        self.square    = square
+        self.direction = direction
+        self.grid      = grid
+
+    @property
+    def other_direction(self) -> Direction:
+        return Direction.DOWN if self.direction is Direction.ACROSS else Direction.ACROSS
+
+    def move(self, dx: int, dy: int) -> Cursor:
+        x, y = self.square.coords
+        while True:
+            x += dx
+            y += dy
+            square = self.grid.get(x, y)
+            if square is None:
+                return self
+            elif square.is_white():
+                return Cursor(square, self.direction, self.grid)
+            else:
+                dx = sign(dx)
+                dy = sign(dy)
+
+    def h(self) -> Cursor:
+        return self.move(-1, 0)
+
+    def j(self) -> Cursor:
+        return self.move(0, 1)
+
+    def k(self) -> Cursor:
+        return self.move(0, -1)
+
+    def l(self) -> Cursor:
+        return self.move(1, 0)
+
+    def next_squares(self) -> Iterator[tuple[Square, Direction]]:
+        start = self.square
+        square = start.next[self.direction]
+        while square is not None:
+            yield square, self.direction
+            square = square.next[self.direction]
+        square = self.grid.words[self.other_direction][0].squares[0]
+        while square is not None:
+            yield square, self.other_direction
+            square = square.next[self.other_direction]
+        square = self.grid.words[self.direction][0].squares[0]
+        while square is not start:
+            assert square is not None
+            yield square, self.direction
+            square = square.next[self.direction]
+
+    def prev_squares(self) -> Iterator[tuple[Square, Direction]]:
+        start = self.square
+        square = start.prev[self.direction]
+        while square is not None:
+            yield square, self.direction
+            square = square.prev[self.direction]
+        square = self.grid.words[self.other_direction][-1].squares[-1]
+        while square is not None:
+            yield square, self.other_direction
+            square = square.prev[self.other_direction]
+        square = self.grid.words[self.direction][-1].squares[-1]
+        while square is not start:
+            assert square is not None
+            yield square, self.direction
+            square = square.prev[self.direction]
+
+    def jump_to_next_square(self, condition: Callable[[Square, Direction], bool]) -> Cursor:
+        for square, direction in self.next_squares():
+            if condition(square, direction):
+                return Cursor(square, direction, self.grid)
+        return self
+
+    def jump_to_prev_square(self, condition: Callable[[Square, Direction], bool]) -> Cursor:
+        for square, direction in self.prev_squares():
+            if condition(square, direction):
+                return Cursor(square, direction, self.grid)
+        return self
+
+    def w(self) -> Cursor:
+        return self.jump_to_next_square(lambda square, direction: square is square.word[direction].squares[0])
+
+    def b(self) -> Cursor:
+        return self.jump_to_prev_square(lambda square, direction: square is square.word[direction].squares[0])
+
+    def e(self) -> Cursor:
+        return self.jump_to_next_square(lambda square, direction: square is square.word[direction].squares[-1])
+
+    def ge(self) -> Cursor:
+        return self.jump_to_prev_square(lambda square, direction: square is square.word[direction].squares[-1])

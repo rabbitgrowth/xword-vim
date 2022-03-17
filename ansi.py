ae6bffe3a3c906c277eba6d10f4f69ca70390b4b
Nick Yeow <ytknick@gmail.com>
2022-03-18 00:49:49 +0800

    Use hexadecimal for escape character

--- ansi.py
+++ ansi.py
@@ -1,16 +1,16 @@
 import sys
 
 def enter_alternate_buffer() -> None:
-    sys.stdout.write(f'\033[?1049h')
+    sys.stdout.write(f'\x1b[?1049h')
 
 def leave_alternate_buffer() -> None:
-    sys.stdout.write(f'\033[?1049l')
+    sys.stdout.write(f'\x1b[?1049l')
 
 def clear_screen() -> None:
-    sys.stdout.write(f'\033[2J')
+    sys.stdout.write(f'\x1b[2J')
 
 def move_cursor(x: int, y: int) -> None:
-    sys.stdout.write(f'\033[{y+1};{x+1}H')
+    sys.stdout.write(f'\x1b[{y+1};{x+1}H')
 
 def bold(text: str) -> str:
-    return f'\033[1m{text}\033[0m'
+    return f'\x1b[1m{text}\x1b[0m'
